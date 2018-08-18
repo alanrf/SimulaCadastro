a9e7790dbbf8ece99a96dd33aa8ee9834ac20b42
Alan Fritsch <alanrf@Alans-MacBook-Pro.local>
2018-08-18 01:02:46 -0300

    initial version

--- app/src/main/java/com/alanrf/simulacadastro/MainActivity.kt
+++ app/src/main/java/com/alanrf/simulacadastro/MainActivity.kt
@@ -2,11 +2,25 @@ package com.alanrf.simulacadastro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun validarCampos(): Boolean {
+        return validaSenhas()
+    }
+
+    fun validaSenhas(): Boolean {
+        return edNome.text == edNome.text
     }
 }
