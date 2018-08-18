a826d10cec23e753d3d9073b64d3e306b5a09305
Alan Fritsch <alanrf@Alans-MacBook-Pro.local>
2018-08-18 17:16:32 -0300

    funcionamento basico

--- app/src/main/java/com/alanrf/simulacadastro/ExibeDadosActivity.kt
+++ app/src/main/java/com/alanrf/simulacadastro/ExibeDadosActivity.kt
@@ -2,11 +2,24 @@ package com.alanrf.simulacadastro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_exibe_dados.*
 
 class ExibeDadosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exibe_dados)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        edNome.text = intent.extras.getCharSequence("nome")
+        edDataNascimento.text = intent.extras.getCharSequence("dataNascimento")
+        edProfissao.text = intent.extras.getCharSequence("profissao")
+        edSexo.text = intent.extras.getCharSequence("sexo")
+        edEstadoCivil.text = intent.extras.getCharSequence("estadoCivil")
+        edEmail.text = intent.extras.getCharSequence("email")
+
     }
 }
--- app/src/main/java/com/alanrf/simulacadastro/MainActivity.kt
+++ app/src/main/java/com/alanrf/simulacadastro/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.alanrf.simulacadastro
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,27 +18,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btCadastrar.setOnClickListener{
+            cadastrar()
+        }
     }
 
     fun validarCampos(): Boolean {
-        return validaEmail() && validaSenhas()
+        var b = validaEmail()
+        b = validaSenhas() && b
+        return b
     }
 
     fun validaEmail(): Boolean {
         val b = edEmail.text.contains("@")
-        if (!b) {
-            edEmail.error = "Email não está num formato reconhecido."
-        }
+        if (!b)
+            edEmail.setError(getString(R.string.msg_valida_email))
         return b
     }
 
     fun validaSenhas(): Boolean {
-        val b = edSenha.text == edConfirmarSenha.text
+        val b = edSenha.text.toString() == edConfirmarSenha.text.toString()
 
         if (!b) {
-            val msg = "Senha e Confirmar Senha devem ser iguais"
-            edSenha.error = msg
-            edConfirmarSenha.error = msg
+            val msg = getString(R.string.msg_valida_senha)
+            edSenha.setError(msg)
+            edConfirmarSenha.setError(msg)
         }
         return b
     }
@@ -42,6 +50,23 @@ class MainActivity : AppCompatActivity() {
     fun cadastrar() {
         if (validarCampos()) {
             //TODO chamar a proxima tela e passar os dados do cadastro para a mesma.
+            val it = Intent(this, ExibeDadosActivity::class.java)
+
+
+            it.putExtra("nome", edNome.text)
+            it.putExtra("dataNascimento", edDataNascimento.text)
+            it.putExtra("email", edEmail.text)
+            it.putExtra("estadoCivil", spEstadoCivil.selectedItem.toString())
+
+            if (rgSexo.checkedRadioButtonId == rdMasculino.id)
+                it.putExtra("sexo", "Masculino")
+            else
+                it.putExtra("sexo", "Feminino")
+
+
+
+
+            startActivity(it)
         }
     }
 }
